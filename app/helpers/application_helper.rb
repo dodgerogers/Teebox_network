5ca112d90475a1f937ae51bb683e5f31dbe3fe79
dodgerogers@hotmail.com <andy.rogers@teebox.co>
2013-04-20 11:56:27 -0700

    refactored find_video to deal with nil

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,11 +19,11 @@ module ApplicationHelper
   end
   
   def question_screenshot(question)
-      if question.video_id == 0 
-        "video_screen.png"
+      if question.video_id == 0  then "video_screen.png"
       else
         v = Video.find_by_id(question.video_id.to_i)
-        v.screenshot 
+        if v.nil? then "video_screen.png" else v.screenshot 
+        end
     end
   end
 end
