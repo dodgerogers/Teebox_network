ddba51b164bab248b6167ebce3c89ed6faa88d84
Andy <dodgerogers@hotmail.com>
2013-07-16 09:06:05 -0700

    hide votes form for non current users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,8 @@ class Ability
   def initialize(user)
     user ||= User.new #not logged in
     can [:new, :create, :read, :vote], :all
+    
+    #Questions
     can [:highest_votes, :unanswered], Question
     can [:update, :destroy, :edit, :create, :new], Question do |question|
       question.try(:user) == user
