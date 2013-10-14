b53333adbeadf42fa1425d03e3782deb9a9b465d
Andy <dodgerogers@hotmail.com>
2013-10-14 16:05:11 -0400

    capistrano deploy + unicorn gems

--- Gemfile
+++ Gemfile
@@ -54,19 +54,15 @@ gem "populator"
 gem "faker"
 gem "pg_search"
 gem 'obscenity'
-
+gem "capistrano"
+gem "rvm-capistrano"
+gem "unicorn"
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
 # To use Jbuilder templates for JSON
 # gem 'jbuilder'
-
-# Use unicorn as the app server
-# gem 'unicorn'
-
-# Deploy with Capistrano
-# gem 'capistrano'
 
 # To use debugger
 # gem 'ruby-debug19', :require => 'ruby-debug'
---
+++ config/deploy.rb
@@ -0,0 +1,27 @@
+require "bundler/capistrano"
+
+server "162.243.40.138", :web, :app, :db, primary: true
+
+set :application, "Teebox_network"
+set :user, "andrew"
+set :scm, "git"
+set :scm_passphrase, ""
+set :repository, "https://github.com/dodgerogers/#{application}"
+set :branch, "master"
+
+set :deploy_to, "/home/rails/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
+
+namespace :deploy do
+  task :restart, roles: :app do
+      run "touch #{current_path}tmp/restart.txt"
+    end
+  end
+  after :finishing, 'deploy:cleanup'
+end
