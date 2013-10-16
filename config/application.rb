9653c91c0c06e8c821d911f691dc1fa42f730135
Andy <dodgerogers@hotmail.com>
2013-10-16 00:40:45 -0400

    fixed autoload in app.rb

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module TeeboxNetwork
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += %W(#{config.root}/decorators)
     config.autoload_paths += %W(#{config.root}/lib)
-    config.autoload_paths += %W(#{config.root}/lib/commentable.rb)
+    config.autoload_paths += %W(#{config.root}/lib//teebox/commentable.rb)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
