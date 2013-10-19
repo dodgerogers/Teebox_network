85afeb4d4d8869839e28dc663b76818569923820
Andy <dodgerogers@hotmail.com>
2013-10-19 15:46:27 -0400

    nil screenshot video present error fix

--- app/helpers/question_helper.rb
+++ app/helpers/question_helper.rb
@@ -3,4 +3,13 @@ module QuestionHelper
   	content_tag(:h2, options, class: "zero-margin") +
   	content_tag(:div, "#{pluralize(questions.total_entries, "Question")} found")
   end 
+  
+  def display_video(question)
+    if question.video.present?
+      screenshot_img = question.video.screenshot.present? ? question.video.screenshot_url(:mini) : "video_screen.jpg"
+    else
+      screenshot_img = "video_screen.jpg"
+    end
+      link_to image_tag(screenshot_img), question
+  end
 end
