801a8e6038899283e331293033b9ac197d17a341
Andy <dodgerogers@hotmail.com>
2013-10-23 13:37:46 -0400

    rm ENV production, replaced with CONFIG

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,8 +22,8 @@ TeeboxNetwork::Application.configure do
    :address => "smtp.gmail.com",
    :port => 587,
    :domain => "gmail.com",
-   :user_name => ENV["EMAIL_USERNAME"],
-   :password => ENV["EMAIL_PASSWORD"],
+   :user_name => CONFIG[:email_username],
+   :password => CONFIG[:email_password],
    :authentication => "plain",
    :enable_starttls_auto => true
   }
