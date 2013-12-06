c0a3d25f0b501431ba05bd6742b093ce73725949
Andy <dodgerogers@hotmail.com>
2013-12-06 13:21:01 -0500

    vote form and js clean up

--- app/assets/javascripts/vote.js.coffee
+++ app/assets/javascripts/vote.js.coffee
@@ -1,9 +1,3 @@
 $("a#vote").click ->
   url = @href
   $.post url
-
-
-$ ->
-  vote = $('#vote-box').text
-  if vote < 0
-    vote.css('color', 'red')
--- lib/amazon_s3.rb
+++ lib/amazon_s3.rb
@@ -2,7 +2,7 @@ class AmazonS3
   
   # Can move screenshots and videos into 1 method
   def self.get_screenshots
-     screenshots = Video.all.map(&:screenshot).map {|file| file.to_s.gsub!("https://teebox-network.s3.amazonaws.com/", "") }
+     screenshots = Video.all.map(&:screenshot).map {|file| file.to_s.gsub!("https://#{CONFIG[:s3_bucket]}.s3.amazonaws.com/", "") }
      files = []
      screenshots.each do |object| 
        ar = File.split(object)
@@ -15,11 +15,11 @@ class AmazonS3
    end
 
    def self.get_videos
-     Video.all.map(&:file).each {|file| file.gsub!("http://teebox-network.s3.amazonaws.com/", "") }
+     Video.all.map(&:file).each {|file| file.gsub!("http://#{CONFIG[:s3_bucket]}.s3.amazonaws.com/", "") }
    end
 
    def self.cleanup
-     bucket = AWS::S3.new.buckets['teebox-network'].objects
+     bucket = AWS::S3.new.buckets[CONFIG[:s3_bucket]].objects
      files = self.get_screenshots.zip(self.get_videos).flatten.compact
      number_of_files = (bucket.map(&:key) - files)
      if number_of_files.size > 0
