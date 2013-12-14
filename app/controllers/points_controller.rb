47f572da10f6781fb3c5348fec797a80911747b7
Andy <dodgerogers@hotmail.com>
2013-12-14 17:15:11 -0500

    user points index fix

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -1,6 +1,6 @@
 class PointsController < ApplicationController
   def index
     @points = Point.find_points(current_user).paginate(page: params[:page], per_page: 12)
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
   end
 end
