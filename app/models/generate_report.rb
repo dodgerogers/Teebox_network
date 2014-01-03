b9328298569176d026fb3d458a890e7ee51ff158
Andy <dodgerogers@hotmail.com>
2014-01-03 18:56:47 -0500

    generate_report class refactor

--- app/models/generate_report.rb
+++ app/models/generate_report.rb
@@ -1,33 +1,19 @@
 class GenerateReport < Report
   def initialize(report)
     @report = report
+    @models = ["question", "answer", "user"]
   end
   
   def create
     @report.class.transaction do
-      set_records
-      set_totals
+      @models.each do |attribute|
+        @report.send("#{attribute.pluralize}"+ "=", record_query(attribute))
+        @report.send("#{attribute.pluralize}_total" + "=", attribute.classify.constantize.all.size)
+      end
     end
-    @report.save!
   end
   
   private 
-  
-  def set_records
-    @report.class.transaction do
-      @report.questions = record_query("question")
-      @report.answers = record_query("answer")
-      @report.users = record_query("user")
-    end
-  end
-  
-  def set_totals
-    @report.class.transaction do
-      @report.answers_total = Answer.all.size
-      @report.questions_total = Question.all.size
-      @report.users_total = User.all.size
-    end  
-  end
   
   def record_query(object)
     object.classify.constantize.where("created_at > ?", 1.day.ago).size
