52edcf6e6bf73eb868bfd7ca576c644825c3a15d
Andy <dodgerogers@hotmail.com>
2014-02-02 00:19:12 -0500

    before + after fork db options

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,5 +5,20 @@ stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
 listen "/tmp/unicorn.teebox.sock"
-worker_processes 2
-timeout 30
+worker_processes 1
+timeout 20
+preload_app true
+
+before_fork do |server, worker|
+  # Disconnect since the database connection will not carry over
+  if defined? ActiveRecord::Base
+    ActiveRecord::Base.connection.disconnect!
+  end
+end
+
+after_fork do |server, worker|
+  # Start up the database connection again in the worker
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
+end
