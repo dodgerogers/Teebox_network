c03fdef9b5c597844ec56f0688b7355bd9cdacfe
Andy <dodgerogers@hotmail.com>
2014-02-02 15:48:04 -0500

    preload_app unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -6,17 +6,17 @@ stdout_path "#{root}/log/unicorn.log"
 
 listen "/tmp/unicorn.teebox.sock"
 worker_processes 2
-#preload_app true
+preload_app true
 timeout 30
 
-# before_fork do |server, worker|
-#   if defined? ActiveRecord::Base
-#     ActiveRecord::Base.connection.disconnect!
-#   end
-# end
-# 
-# after_fork do |server, worker|
-#   if defined?(ActiveRecord::Base)
-#     ActiveRecord::Base.establish_connection
-#   end
-# end
+before_fork do |server, worker|
+  if defined? ActiveRecord::Base
+    ActiveRecord::Base.connection.disconnect!
+  end
+end
+
+after_fork do |server, worker|
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
+end
