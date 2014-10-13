e364135c471425696f8054681c5e026b5ba442a1
Andy <dodgerogers@hotmail.com>
2014-10-13 18:14:20 -0400

    Rm delay on TranscoderRepo#generate

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -23,12 +23,11 @@ class VideosController < ApplicationController
     Rails.logger.info("*~*~*~*~* Creating video...")
     respond_to do |format|
       if @video.save
-        TranscoderRepository.delay.generate(@video)
+        TranscoderRepository.generate(@video)
         Rails.logger.info("*~*~*~*~* Created Transcoder Job, new video attrs - #{@video.attributes.slice("file", "screenshot")}...")
         format.html { redirect_to videos_path, notice: 'Video was successfully uploaded.' }
         format.json { render json: @video, status: :created, location: @video }
         format.js 
-        Rails.logger.info("*~*~*~*~* Finished rendering.")
       else
         format.html { render action: "new" }
         format.json { render json: @video.errors, status: :unprocessable_entity }
